e5cefa0e313e47ddf343f45bde5338c25dcb7fd1
Grayson Gantek <graysonleegantek@gmail.com>
2019-07-24 18:36:41 -0400

    Added slideshow of images from server

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,24 +1,13 @@
-// const results = $.ajax({
-// 	url: "https://omdbapi.com/?apikey=trilogy&s=star%20wars",
-// 	method: "GET"
-// }).then(function(results) {
-// console.log(results);
-// });
-
 let servVersion, servHostName, servMOTD;
 
+const servResults = $.ajax({
+    url: "https://api.mcsrvstat.us/2/playodyssey.net",
+    method: "GET"
+}).then(function (stats) {
+    servVersion = stats.version;
+    servHostName = stats.hostname;
+    servMOTD = stats.motd.clean;
 
-// const servResults = $.ajax({
-//     url: "https://api.mcsrvstat.us/2/playodyssey.net",
-//     method: "GET"
-// }).then(function (stats) {
-//     console.log(stats);
-//     servVersion = stats.version;
-//     servHostName = stats.hostname;
-//     servMOTD = stats.motd.clean;
+    $("#playerCount").text(`${stats.players.online} / ${stats.players.max} players online.`);
 
-//     $("#mainCardSubtitle").text(`${stats.players.online} / ${stats.players.max}`);
-
-// });
-
-// $("body").append(servVersion, servHostName, servMOTD);
+});
