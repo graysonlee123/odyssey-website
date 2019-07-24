4385e658661b24bdbf7c0f4d28eb32b835a5a538
Grayson Gantek <graysonleegantek@gmail.com>
2019-07-24 16:19:03 -0400

    Added info card with Minecraft Server Stats

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,1 +1,24 @@
-console.log("Hello World");
+// const results = $.ajax({
+// 	url: "https://omdbapi.com/?apikey=trilogy&s=star%20wars",
+// 	method: "GET"
+// }).then(function(results) {
+// console.log(results);
+// });
+
+let servVersion, servHostName, servMOTD;
+
+
+const servResults = $.ajax({
+    url: "https://api.mcsrvstat.us/2/playodyssey.net",
+    method: "GET"
+}).then(function (stats) {
+    console.log(stats);
+    servVersion = stats.version;
+    servHostName = stats.hostname;
+    servMOTD = stats.motd.clean;
+
+    $("#mainCardSubtitle").text(`${stats.players.online} / ${stats.players.max}`);
+
+});
+
+// $("body").append(servVersion, servHostName, servMOTD);
